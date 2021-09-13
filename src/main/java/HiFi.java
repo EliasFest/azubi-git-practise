ccc6827981c7c7f3c0536b9e1355fcfd637ce4df
dxicqrf <dxicqrf@audiiny23843800.fritz.box>
2021-09-13 10:11:07 +0200

    new method name

--- src/main/java/HiFi.java
+++ src/main/java/HiFi.java
@@ -1,18 +1,18 @@
 public class HiFi {
 
-    private boolean isTurnedOn = false;
+    private boolean isHalloTurned = false;
     private boolean standbyLedIndicator = true;
     private String status;
 
     public String turnOn() {
-        isTurnedOn = true;
+        isHalloTurned = true;
         standbyLedIndicator = false;
         status = "Device is online and not running in standby";
         return status;
     }
 
     public String turnOff() {
-        isTurnedOn = false;
+        isHalloTurned = false;
         standbyLedIndicator = true;
         status = "Device is offline but standby is enabled";
         return status;
